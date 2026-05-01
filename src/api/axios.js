5e193b7d652855cd74546f8e263f5e88ddf984a0
Josh-Catague <jscatague@ccc.edu.ph>
2026-05-01 16:13:45 +0800

    update
    update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost/api' })
+const api = axios.create({ baseURL: 'https://joshua.catague.app/api' })
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
