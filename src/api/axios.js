8988971e099744c99603ee01c8ba4ca9cd0b2c56
Josh-Catague <jscatague@ccc.edu.ph>
2026-05-04 21:08:49 +0800

    WW
    WW

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({ baseURL: '/api' })
+// const api = axios.create({ baseURL: '/api' })
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
